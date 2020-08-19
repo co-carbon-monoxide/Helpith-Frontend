adf47705ff5e941859e876362d84117814d2bc95
Momeemt <momiimt@gmail.com>
2020-08-19 20:13:08 +0900

    🎉 Init Project!

---
+++ app/src/main/java/monoxide/carbon/Helpith/MainActivity.kt
@@ -0,0 +1,12 @@
+package monoxide.carbon.Helpith
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
