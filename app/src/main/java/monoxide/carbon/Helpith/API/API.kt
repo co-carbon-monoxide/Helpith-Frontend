fd93de29e0c83b483c33a62f9b3b7a684a42e33e
Momeemt <momiimt@gmail.com>
2020-08-24 23:00:35 +0900

    👍 Complete: API通信を実装
    localhostから直接通信できるようにした

---
+++ app/src/main/java/monoxide/carbon/Helpith/API/API.kt
@@ -0,0 +1,91 @@
+package monoxide.carbon.Helpith.API
+
+import android.os.AsyncTask
+import org.json.JSONException
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+
+private fun getCompleteUrl(url: String): String {
+    val rootPath = "http://10.0.2.2:3000/api/v1/"
+    return rootPath + url
+}
+
+class API (private val controllerName: String) {
+    fun index () {
+        HitAPITask().execute(getCompleteUrl(controllerName), "GET")
+    }
+
+    fun show (id: String) {
+        HitAPITask().execute(getCompleteUrl("$controllerName/$id"), "GET")
+    }
+
+    fun create () {
+        HitAPITask().execute(getCompleteUrl(controllerName), "POST")
+    }
+
+    fun update (id: String) {
+        HitAPITask().execute(getCompleteUrl("$controllerName/$id"), "PATCH")
+    }
+
+    fun destroy (id: String) {
+        HitAPITask().execute(getCompleteUrl("$controllerName/$id"), "DELETE")
+    }
+}
+
+class HitAPITask: AsyncTask<String, String, String>() {
+    override fun doInBackground(vararg params: String?): String? {
+        var connection: HttpURLConnection? = null
+        var reader: BufferedReader? = null
+        val buffer: StringBuffer
+
+        try {
+            val url = URL(params[0])
+            val httpMethod = params[1]
+            connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = httpMethod
+            connection.connect()
+
+            val stream = connection.inputStream
+            reader = BufferedReader(InputStreamReader(stream))
+            buffer = StringBuffer()
+            var line: String?
+            while (true) {
+                line = reader.readLine()
+                if (line == null) {
+                    break
+                }
+                buffer.append(line)
+            }
+
+            val jsonText = buffer.toString()
+            return jsonText
+        } catch (e: MalformedURLException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+
+        finally {
+            connection?.disconnect()
+            try {
+                reader?.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+
+        return null
+    }
+
+    override fun onPostExecute(result: String?) {
+        super.onPostExecute(result)
+        if (result == null) return
+        println(result)
+    }
+}
--- app/src/main/java/monoxide/carbon/Helpith/MainActivity.kt
+++ app/src/main/java/monoxide/carbon/Helpith/MainActivity.kt
@@ -1,29 +1,19 @@
 package monoxide.carbon.Helpith
 
-import android.os.AsyncTask
 import android.os.Bundle
-import android.view.View
 import android.content.Intent
 import android.widget.Button
 import android.widget.CalendarView
-import android.widget.CalendarView.OnDateChangeListener
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import org.json.JSONException
-import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.MalformedURLException
-import java.net.URL
-
+import monoxide.carbon.Helpith.API.API
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tasksAPI = API("tasks")
 
         val calenderView: CalendarView = findViewById(R.id.calendarView)
         val listener = DateChangeListener()
@@ -35,6 +25,10 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        val hitAPIButton: Button = findViewById(R.id.hit_api_button)
+        hitAPIButton.setOnClickListener {
+            tasksAPI.index()
+        }
     }
 
     private inner class DateChangeListener : CalendarView.OnDateChangeListener {
@@ -45,68 +39,4 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
-
-    inner class HitAPITask: AsyncTask<String, String, String>() {
-        override fun doInBackground(vararg params: String?): String? {
-            var connection: HttpURLConnection? = null
-            var reader: BufferedReader? = null
-            val buffer: StringBuffer
-
-
-            try {
-                val url = URL(params[0])
-                connection = url.openConnection() as HttpURLConnection
-                connection.connect()
-
-                val stream = connection.inputStream
-                reader = BufferedReader(InputStreamReader(stream))
-                buffer = StringBuffer()
-                var line: String?
-                while (true) {
-                    line = reader.readLine()
-                    if (line == null) {
-                        break
-                    }
-                    buffer.append(line)
-                }
-
-                val jsonText = buffer.toString()
-
-                val parentJsonObj = JSONObject(jsonText)
-
-                val parentJsonArray = parentJsonObj.getJSONArray("movies")
-
-                val detailJsonObj = parentJsonArray.getJSONObject(0)
-
-                val movieName: String = detailJsonObj.getString("title")
-
-                val year: Int = detailJsonObj.getInt("year")
-
-                return "$movieName - $year"
-            } catch (e: MalformedURLException) {
-                e.printStackTrace()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            } catch (e: JSONException) {
-                e.printStackTrace()
-            }
-
-            finally {
-                connection?.disconnect()
-                try {
-                    reader?.close()
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
-            }
-
-            return null
-        }
-    }
-
-//    override fun onPostExecute(result: String?) {
-//        super.onPostExecute(result)
-//        if (result == null) return
-//        textView.text = result
-//    }
 }
