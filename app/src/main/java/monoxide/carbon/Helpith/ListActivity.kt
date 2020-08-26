966ef1b271377d324cda1bde8c62fca17aceca20
Momeemt <momiimt@gmail.com>
2020-08-26 23:14:26 +0900

    🚧 wip: 動的にリストに要素を追加する
    まっっっったく思った通りに動かなくて死ぬほどイライラした

--- app/src/main/java/monoxide/carbon/Helpith/ListActivity.kt
+++ app/src/main/java/monoxide/carbon/Helpith/ListActivity.kt
@@ -1,26 +1,48 @@
 package monoxide.carbon.Helpith
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.CalendarView
-import android.widget.TextView
+import android.text.Layout
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class ListActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list)
+
+        val activityList = layoutInflater.inflate(R.layout.activity_list, null)
 
         val date = intent.getStringExtra("HELPITH_DATE")
-        val helpithListDataTextView: TextView = findViewById(R.id.helpith_list_date)
+        val helpithListDataTextView: TextView = activityList.findViewById(R.id.helpith_list_date)
         helpithListDataTextView.text = date
 
-        val button: Button = findViewById(R.id.button)
+        val button: Button = activityList.findViewById(R.id.button)
         button.setOnClickListener {
             finish()
         }
+
+        // val tableRowHeader: TableRow = activityList.findViewById(R.id.helpith_list_table_row_header)
+        // val helpithListTable = layoutInflater.inflate(R.layout.helpith_list_table_row_text, null)
+
+        val tableLayout: TableLayout = activityList.findViewById(R.id.tableLayout)
+        val tableRowHeaderParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+
+        val tableRowHeader = TableRow(this).also {
+            val textView = TextView(this)
+            val textViewParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+            textViewParams.weight = 1f
+            textView.text = "メンバー"
+            it.addView(textView, textViewParams)
+        }
+
+        tableLayout.addView(tableRowHeader, tableRowHeaderParams)
+
+        val tv = TextView(this)
+        tv.text = "ttttt"
+        activityList.findViewById<ConstraintLayout>(R.id.helpith_list_constraint_layout).addView(tv)
+
+        setContentView(activityList)
     }
 
 }
