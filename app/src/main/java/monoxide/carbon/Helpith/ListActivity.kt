3d0c808a0dc4bb4ee2d4da218bc32977418745d7
Momeemt <momiimt@gmail.com>
2020-08-29 17:36:23 +0900

    🚧 wip: その日の家事を追加する機能を実装中

--- app/src/main/java/monoxide/carbon/Helpith/ListActivity.kt
+++ app/src/main/java/monoxide/carbon/Helpith/ListActivity.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.gson.JsonObject
 import monoxide.carbon.Helpith.API.*
 import org.json.JSONArray
 import org.json.JSONObject
@@ -15,74 +16,118 @@ import kotlin.concurrent.thread
 
 class ListActivity: AppCompatActivity() {
 
+    val handler = Handler()
+    var userSize = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        /*
+        *   やらなきゃいけないこと
+        *　　・日付からその日のリストを取得
+        *   ・ボタンが押されたら家事を追加
+        * 　 ・マス目のボタンが押されたら家事タスクの done/not done
+         */
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
 
-        val activityList = layoutInflater.inflate(R.layout.activity_list, null)
-
+        // 日付をセット
         val date = intent.getStringExtra("HELPITH_DATE")
-        val helpithListDataTextView: TextView = activityList.findViewById(R.id.helpith_list_date)
+        val helpithListDataTextView: TextView = findViewById(R.id.helpith_list_date)
         helpithListDataTextView.text = date
+        // end
 
-        val button: Button = activityList.findViewById(R.id.button)
-        button.setOnClickListener {
-            finish()
-        }
-
-        val viewGroup = activityList.findViewById<View>(R.id.tableLayout) as ViewGroup
+        // 家族をセット
+        val viewGroup = findViewById<View>(R.id.tableLayout) as ViewGroup
         val tableRow = TableRow(this) as ViewGroup
         layoutInflater.inflate(R.layout.helpith_list_table_row_text, tableRow)
         val textView = tableRow.getChildAt(0) as TextView
         textView.text = "メンバー"
 
-        val handler = Handler()
+        val userNames = intent.getStringArrayExtra("USER_NAMES") ?: throw error("Empty USER_NAMES")
+        userSize = userNames.size
+        for (i in 0 until userSize) {
+            layoutInflater.inflate(R.layout.helpith_list_table_row_text, tableRow)
+            val nameTextView = tableRow.getChildAt(i + 1) as TextView
+            nameTextView.text = userNames[i]
+        }
+        viewGroup.addView(tableRow)
+        // end
 
-        setContentView(activityList)
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            finish()
+        }
 
+        if (date != null) {
+            displayHelpithList(date)
+        }
+    }
+
+    fun displayHelpithList (date: String) {
         thread {
-            val familyAPI = FamilyAPI()
-            val myFamily = familyAPI.show(5) ?: throw error("empty family!")
-            val familyJson = JSONObject(myFamily)
-            val users = familyJson.getJSONArray("users")
-
             val listAPI = ListAPI()
             val replacedDate = date.replace('/', '-')
-            val list = listAPI.showByDate(5, replacedDate)
-
-            for (i in 0 until users.length()) {
-                val user = users[i] as JSONObject
-                val name = user.optString("name") ?: throw error("valid error name")
-
-                handler.post( Runnable() {
-                    layoutInflater.inflate(R.layout.helpith_list_table_row_text, tableRow)
-                    val nameTextView = tableRow.getChildAt(i + 1) as TextView
-                    nameTextView.text = name
-                })
-            }
-
-            val houseWorkAPI = HouseWorkAPI()
-            val houseWorkEditText: EditText = findViewById(R.id.house_work_edit_text)
-            val addHouseWorkButton: Button = findViewById(R.id.add_house_work_button)
+            val todayList = listAPI.showByDate(5, replacedDate)
+            val listJson = JSONObject(todayList)
+            println(listJson)
+            val houseWorks = listJson.getJSONArray("house_works")
+            val listId = listJson.optString("id").toInt()
+            setAddHouseWork(listId)
 
             handler.post( Runnable () {
-                addHouseWorkButton.setOnClickListener {
-                    val text = houseWorkEditText.text
-                    val houseWorkRequest = HouseWorkRequest(
-                        name = "testHouseWork",
-                        time = 20,
-                        list_id = 1
-                    )
-                    if (!TextUtils.isEmpty(text)) {
-                        println("記入されています: $text")
-                        thread {
-                            houseWorkAPI.create(houseWorkRequest)
-                        }
-                        houseWorkEditText.text.clear()
+                val viewGroup = findViewById<View>(R.id.tableLayout) as ViewGroup
+                for (i in 0 until houseWorks.length()) {
+                    val houseWork = houseWorks[i] as JSONObject
+                    val houseWorkName = houseWork.optString("name")
+                    val tableRow = TableRow(this) as ViewGroup
+                    layoutInflater.inflate(R.layout.helpith_list_table_row_text, tableRow)
+                    val textView = tableRow.getChildAt(0) as TextView
+                    textView.text = houseWorkName
+
+                    for (j in 0 until userSize) {
+                        layoutInflater.inflate(R.layout.helpith_list_table_row_text, tableRow)
+                        val emptyTextView = tableRow.getChildAt(j + 1) as TextView
+                        emptyTextView.text = ""
                     }
+                    viewGroup.addView(tableRow)
                 }
             })
         }
+    }
 
-        viewGroup.addView(tableRow)
+    fun setAddHouseWork (listId: Int) {
+        val houseWorkAPI = HouseWorkAPI()
+        val houseWorkEditText: EditText = findViewById(R.id.house_work_edit_text)
+        val addHouseWorkButton: Button = findViewById(R.id.add_house_work_button)
+        val viewGroup = findViewById<View>(R.id.tableLayout) as ViewGroup
+        handler.post( Runnable () {
+            addHouseWorkButton.setOnClickListener {
+                val text = houseWorkEditText.text
+                val houseWorkRequest = HouseWorkRequest(
+                    name = text.toString(),
+                    time = 20,
+                    list_id = listId
+                )
+                if (!TextUtils.isEmpty(text)) {
+                    thread {
+                        houseWorkAPI.create(houseWorkRequest)
+                    }
+
+                    val tableRow = TableRow(this) as ViewGroup
+                    layoutInflater.inflate(R.layout.helpith_list_table_row_text, tableRow)
+                    val textView = tableRow.getChildAt(0) as TextView
+                    textView.text = text.toString()
+
+                    println("-------------------------------- $userSize")
+                    for (i in 0 until userSize) {
+                        layoutInflater.inflate(R.layout.helpith_list_table_row_text, tableRow)
+                        val emptyTextView = tableRow.getChildAt(i + 1) as TextView
+                        emptyTextView.text = ""
+                    }
+                    viewGroup.addView(tableRow)
+
+                    houseWorkEditText.text.clear()
+                }
+            }
+        })
     }
 }
--- app/src/main/java/monoxide/carbon/Helpith/MainActivity.kt
+++ app/src/main/java/monoxide/carbon/Helpith/MainActivity.kt
@@ -5,26 +5,54 @@ import android.content.Intent
 import android.widget.Button
 import android.widget.CalendarView
 import androidx.appcompat.app.AppCompatActivity
-import monoxide.carbon.Helpith.API.HelpithAPI
+import monoxide.carbon.Helpith.API.FamilyAPI
+import org.json.JSONObject
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+
+    var userNames: Array<String?> = arrayOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val family_id = 5
+
+        val familyAPI = FamilyAPI()
+        thread {
+            val myFamily = familyAPI.show(family_id) ?: throw error("empty family!")
+            val familyJson = JSONObject(myFamily)
+            val usersJson = familyJson.getJSONArray("users")
+            for (i in 0 until usersJson.length()) {
+                val user = usersJson[i] as JSONObject
+                val name = user.optString("name")
+                userNames += name
+            }
+        }
+
+        setCalenderViewClickEvent()
+        openConfigActivityByClickEvent()
+        openEvaluationActivityByClickEvent()
+    }
+
+    private fun setCalenderViewClickEvent () {
         val calenderView: CalendarView = findViewById(R.id.calendarView)
         val listener = DateChangeListener()
         calenderView.setOnDateChangeListener(listener)
+    }
 
+    private fun openConfigActivityByClickEvent () {
         val configButton: Button = findViewById(R.id.config_button)
         configButton.setOnClickListener {
             val intent = Intent(applicationContext, ConfigActivity::class.java)
             startActivity(intent)
         }
+    }
 
+    private fun openEvaluationActivityByClickEvent () {
         val evaluationButton: Button = findViewById(R.id.evaluation_button)
-        evaluationButton.setOnClickListener{
+        evaluationButton.setOnClickListener {
             val intent = Intent(applicationContext, EvaluationActivity::class.java)
             startActivity(intent)
         }
@@ -35,6 +63,7 @@ class MainActivity : AppCompatActivity() {
             val displayMonth = month + 1
             val intent = Intent(applicationContext, ListActivity::class.java)
             intent.putExtra("HELPITH_DATE", "$year/$displayMonth/$dayOfMonth")
+            intent.putExtra("USER_NAMES", userNames)
             startActivity(intent)
         }
     }
